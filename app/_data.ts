5171154fe4a9ee586f5fd38c4b121f0953964281
Akaikenlol <keithken972@gmail.com>
2024-02-06 21:35:23 +0630

    styling: add css properties

---
+++ app/_data.ts
@@ -0,0 +1,26 @@
+export const data = [
+	{
+		name: "Horn",
+		rarity: 6,
+		image: [
+			{
+				name: "Base",
+				link: "https://ucarecdn.com/8b0922b5-185e-42b7-89e1-a355d43c4527/",
+			},
+			{
+				name: "E1",
+				link: "https://ucarecdn.com/dcee8a66-2bfe-499a-a4fc-7bfe8b47593e/",
+			},
+			{
+				name: "E2",
+				link: "https://ucarecdn.com/6d421b80-5ece-4841-98d2-625692a61f9f/",
+			},
+		],
+		availability: "Global",
+		affiliation: "Victoria",
+		class: ["Defender", "Fortress"],
+		lore: {
+			combat_experience: "9 Years",
+		},
+	},
+];
---
+++ components/Hero.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import React from "react";
+
+const Hero = () => {
+	return (
+		<header className="bg-center bg-cover bg-no-repeat sm:p-16 py-16 px-8 flex justify-center lg:items-center max-lg:flex-col w-full sm:gap-16 gap-0">
+			<div className="flex-1 flex flex-col gap-10">
+				<Image
+					src={"/assets/logo.svg"}
+					alt="logo"
+					width={101}
+					height={101}
+					className="object-contain"
+				/>
+				<h1 className="sm:text-6xl text-5xl text-white lg:max-w-lg font-bold leading-loose">
+					Explore The <span className="">Divers Realms</span> of Rhodes Island
+				</h1>
+			</div>
+			<div className="lg:flex-1 relative w-full h-[50vh] justify-center">
+				<Image
+					src={"/assets/RI.svg"}
+					alt="mostima"
+					width={120}
+					height={120}
+					className="object-contain"
+				/>
+			</div>
+		</header>
+	);
+};
+
+export default Hero;
