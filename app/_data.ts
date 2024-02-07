c02ba1ea71f2e1982c5730866f1b0bf886439778
Akaikenlol <keithken972@gmail.com>
2024-02-07 20:19:58 +0630

    feat: Operator Card chore: Operator Mock Data style: Icon add

--- app/_data.ts
+++ app/_data.ts
@@ -1,26 +1,230 @@
 export const data = [
 	{
-		name: "Horn",
+		name: "Ling",
 		rarity: 6,
 		image: [
 			{
 				name: "Base",
-				link: "https://ucarecdn.com/8b0922b5-185e-42b7-89e1-a355d43c4527/",
+				link: "https://res.cloudinary.com/du7mimevn/image/upload/v1705726730/char_2023_ling_1.png",
 			},
 			{
 				name: "E1",
-				link: "https://ucarecdn.com/dcee8a66-2bfe-499a-a4fc-7bfe8b47593e/",
+				link: "https://res.cloudinary.com/du7mimevn/image/upload/v1705726731/char_2023_ling_1_0.png",
 			},
 			{
 				name: "E2",
-				link: "https://ucarecdn.com/6d421b80-5ece-4841-98d2-625692a61f9f/",
+				link: "https://res.cloudinary.com/du7mimevn/image/upload/v1705726731/char_2023_ling_2.png",
+			},
+			{
+				name: "It Does Wash the Strings",
+				link: "https://res.cloudinary.com/du7mimevn/image/upload/v1705726734/char_2023_ling_nian_239.png",
+			},
+			{
+				name: "Towering is Cliff of Nostalgia",
+				link: "https://res.cloudinary.com/du7mimevn/image/upload/v1705726737/E7_AB_8B_E7_BB_98__E4_BB_A4_skin2.png",
 			},
 		],
 		availability: "Global",
-		affiliation: "Victoria",
+		affiliation: "Yan",
 		class: ["Defender", "Fortress"],
 		lore: {
 			combat_experience: "9 Years",
+		},
+	},
+	{
+		name: "Ines",
+		rarity: 6,
+		image: [
+			{
+				name: "Base",
+				link: "https://res.cloudinary.com/du7mimevn/image/upload/v1705726518/E7_AB_8B_E7_BB_98__E4_BC_8A_E5_86_85_E4_B8_9D_1.png",
+			},
+			{
+				name: "E1",
+				link: "https://res.cloudinary.com/du7mimevn/image/upload/v1705726518/E7_AB_8B_E7_BB_98__E4_BC_8A_E5_86_85_E4_B8_9D_1.png",
+			},
+			{
+				name: "E2",
+				link: "https://res.cloudinary.com/du7mimevn/image/upload/v1705726521/E7_AB_8B_E7_BB_98__E4_BC_8A_E5_86_85_E4_B8_9D_2.png",
+			},
+		],
+		availability: "Global",
+		affiliation: "Rhodes Island",
+		class: ["Vanguard", "Agent"],
+		lore: {
+			combat_experience: "19 Years",
+		},
+	},
+	{
+		name: "W",
+		rarity: 6,
+		image: [
+			{
+				name: "Base",
+				link: "https://res.cloudinary.com/du7mimevn/image/upload/v1705727034/char_113_cqbw_1_0.png",
+			},
+			{
+				name: "E1",
+				link: "https://res.cloudinary.com/du7mimevn/image/upload/v1705727035/char_113_cqbw_1.png",
+			},
+			{
+				name: "E2",
+				link: "https://res.cloudinary.com/du7mimevn/image/upload/v1705727035/char_113_cqbw_2.png",
+			},
+			{
+				name: "Fugue",
+				link: "https://res.cloudinary.com/du7mimevn/image/upload/v1705727038/char_113_cqbw_epoque_237.png",
+			},
+		],
+		availability: "Global",
+		affiliation: "Babel",
+		class: ["Sniper", "AoE", "Artilleryman"],
+		lore: {
+			combat_experience: "11 Years",
+		},
+	},
+	{
+		name: "Nian",
+		rarity: 6,
+		image: [
+			{
+				name: "Base",
+				link: "https://res.cloudinary.com/du7mimevn/image/upload/v1705727080/char_2014_nian_1_0.png",
+			},
+			{
+				name: "E1",
+				link: "https://res.cloudinary.com/du7mimevn/image/upload/v1705727080/char_2014_nian_1_0.png",
+			},
+			{
+				name: "E2",
+				link: "https://res.cloudinary.com/du7mimevn/image/upload/v1705727082/char_2014_nian_2_0.png",
+			},
+			{
+				name: "Unfettered Freedom",
+				link: "https://res.cloudinary.com/du7mimevn/image/upload/v1705727084/char_2014_nian_nian_234-2.png",
+			},
+		],
+		availability: "Global",
+		affiliation: "Yan",
+		class: ["Defender", "Normal", "Protector"],
+		lore: {
+			combat_experience: "Undisclosed",
+		},
+	},
+	{
+		name: "Dusk",
+		rarity: 6,
+		image: [
+			{
+				name: "Base",
+				link: "https://res.cloudinary.com/du7mimevn/image/upload/v1705727102/char_017_huang_1_0.png",
+			},
+			{
+				name: "E1",
+				link: "https://res.cloudinary.com/du7mimevn/image/upload/v1705726891/char_2015_dusk_1_0.png",
+			},
+			{
+				name: "E2",
+				link: "https://res.cloudinary.com/du7mimevn/image/upload/v1705726892/char_2015_dusk_2.png",
+			},
+			{
+				name: "Unfettered Freedom",
+				link: "https://res.cloudinary.com/du7mimevn/image/upload/v1705726895/char_2015_dusk_nian_237-2.png",
+			},
+		],
+		availability: "Global",
+		affiliation: "Yan",
+		class: ["Caster", "AoE", "Splash Caster"],
+		lore: {
+			combat_experience: "Undisclosed",
+		},
+	},
+	{
+		name: "Blaze",
+		rarity: 6,
+		image: [
+			{
+				name: "Base",
+				link: "https://res.cloudinary.com/du7mimevn/image/upload/v1705727102/char_017_huang_1_0.png",
+			},
+			{
+				name: "E1",
+				link: "https://res.cloudinary.com/du7mimevn/image/upload/v1705727102/char_017_huang_1_0.png",
+			},
+			{
+				name: "E2",
+				link: "https://res.cloudinary.com/du7mimevn/image/upload/v1705727103/char_017_huang_2.png",
+			},
+			{
+				name: "Unfettered Freedom",
+				link: "https://res.cloudinary.com/du7mimevn/image/upload/v1705727106/char_017_huang_as_231.png",
+			},
+		],
+		availability: "Global",
+		affiliation: "Rhodes Island",
+		class: ["Guard", "AoE", "Centurion"],
+		lore: {
+			combat_experience: "7 years",
+		},
+	},
+	{
+		name: "Surtr",
+		rarity: 6,
+		image: [
+			{
+				name: "Base",
+				link: "https://res.cloudinary.com/du7mimevn/image/upload/v1705726964/char_350_surtr_1.png",
+			},
+			{
+				name: "E1",
+				link: "https://res.cloudinary.com/du7mimevn/image/upload/v1705726964/char_350_surtr_1_0.png",
+			},
+			{
+				name: "E2",
+				link: "https://res.cloudinary.com/du7mimevn/image/upload/v1705726965/char_350_surtr_2.png",
+			},
+			{
+				name: "Liberté//Échec",
+				link: "https://res.cloudinary.com/du7mimevn/image/upload/v1705726970/char_350_surtr_it_231.png",
+			},
+			{
+				name: "Colorful Wonderland CW03",
+				link: "https://res.cloudinary.com/du7mimevn/image/upload/v1705726972/E7_AB_8B_E7_BB_98__E5_8F_B2_E5_B0_94_E7_89_B9_E5_B0_94_skin2.png",
+			},
+		],
+		availability: "Global",
+		affiliation: "Rhodes Island",
+		class: ["Guard", "Arts Fighter"],
+		lore: {
+			combat_experience: "2 years",
+		},
+	},
+	{
+		name: "Texas the Omertosa",
+		rarity: 6,
+		image: [
+			{
+				name: "Base",
+				link: "https://res.cloudinary.com/du7mimevn/image/upload/v1705726589/E7_AB_8B_E7_BB_98__E7_BC_84_E9_BB_98_E5_BE_B7_E5_85_8B_E8_90_A8_E6_96_AF_1.png",
+			},
+			{
+				name: "E1",
+				link: "https://res.cloudinary.com/du7mimevn/image/upload/v1705726589/E7_AB_8B_E7_BB_98__E7_BC_84_E9_BB_98_E5_BE_B7_E5_85_8B_E8_90_A8_E6_96_AF_1_0.png",
+			},
+			{
+				name: "E2",
+				link: "https://res.cloudinary.com/du7mimevn/image/upload/v1705726590/E7_AB_8B_E7_BB_98__E7_BC_84_E9_BB_98_E5_BE_B7_E5_85_8B_E8_90_A8_E6_96_AF_2.png",
+			},
+			{
+				name: "Wingbreaker",
+				link: "https://res.cloudinary.com/du7mimevn/image/upload/v1705726593/E7_AB_8B_E7_BB_98__E7_BC_84_E9_BB_98_E5_BE_B7_E5_85_8B_E8_90_A8_E6_96_AF_skin1.png",
+			},
+		],
+		availability: "Global",
+		affiliation: "Penguin Logistics",
+		class: ["Specialist", "Fast-Redeploy", "Executor"],
+		lore: {
+			combat_experience: "5 years",
 		},
 	},
 ];
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 import Image from "next/image";
 import { data } from "./_data";
+import OperatorCard, { OperatorCardProps } from "@/components/Card";
 
 export default function Home() {
 	return (
 		<main className="sm:p-16 py-16 px-8 flex flex-col gap-10">
 			<h2 className="text-3xl text-white font-bold">Explore Operators</h2>
+
+			<section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+				{data.map((item: OperatorCardProps, index) => (
+					<OperatorCard key={item.name} operator={item} index={index} />
+				))}
+			</section>
 		</main>
 	);
 }
