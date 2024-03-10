b1d86f8637b4bb1dd91a50e45ef560f7ffdb6ffb
Akaikenlol <keithken972@gmail.com>
2024-03-10 21:40:47 +0630

    feat: ProfileInfoCard chore: npm update, batch, refactoring profile page

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,53 +1,36 @@
-import { fetchOperatorByName, fetchOperatorImage } from "@/lib/actions/actions";
+import { fetchOperatorByName } from "@/lib/actions/actions";
+import { Badge } from "@/components/ui/badge";
 import { ProfilePageProps } from "@/types";
 import Image from "next/image";
+import ProfileInfoCard from "@/components/shared/ProfileInfoCard";
 
 const ProfilePage = async ({ params }: ProfilePageProps) => {
 	const { id } = params;
 	const data = await fetchOperatorByName(id);
 
 	return (
-		<div className="text-center flex-center flex-col bg-gradient-to-t from-amber-50 to-cyan-50 h-[100vh] gap-3">
-			<h1 className="text-black text-2xl">{data.name}</h1>
-			{/* <div className="flex justify-center items-center flex-col gap-2 mt-2 w-[500px] h-[650px] bg-gradient-to-t from-cyan-100  to-yellow-100 rounded-md">
-				<Image
-					src={
-						data.art && data.art[0] ? data.art[0].link : "/assets/mostima.png"
-					}
-					alt="Operator Profile Picture"
-					className="object-cover"
-					width={500}
-					height={500}
-					priority
-				/>
-				<div className="flex flex-col text-center justify-center items-center gap-2 rounded-md text-black font-semibold">
-					<h2>Operator Name: {data.name}</h2>
-					<p>Rarity: {data.rarity}</p>
-					<p>Release Date Global: {data.release_dates.global}</p>
-					<p>Release Date CN: {data.release_dates.cn}</p>
-				</div>
-			</div> */}
-
-			<div className="flex bg-gradient-to-t from-cyan-100  to-yellow-100 rounded-md mt-2">
-				<Image
-					src={
-						data.art && data.art[0] ? data.art[0].link : "/assets/mostima.png"
-					}
-					alt="Operator Profile Picture"
-					className="object-cover"
-					width={500}
-					height={500}
-					priority
-				/>
-				<div className="h-auto w-1 bg-teal-500/20" />
-				<div className="flex flex-col text-left gap-2 rounded-md text-black font-semibold">
-					<h2>Operator Name: {data.name}</h2>
-					<p>Rarity: {data.rarity}</p>
-					<p>Release Date Global: {data.release_dates.global}</p>
-					<p>Release Date CN: {data.release_dates.cn}</p>
+		<>
+			<div className="text-center flex px-12 py-8 flex-col bg-gradient-to-t from-amber-50 to-cyan-50 h-[100vh] gap-3">
+				<div className="grid lg:grid-cols-2 sm:grid-rows-1 gap-20 max-sm:grid-rows-2">
+					<div className="bg-gradient-to-t from-cyan-100  to-yellow-100 rounded-md py-2">
+						<h2 className="text-black text-2xl">Operator {data.name}</h2>
+						<Image
+							src={
+								data.art && data.art[0]
+									? data.art[0].link
+									: "/assets/mostima.png"
+							}
+							alt="Operator Profile Picture"
+							className="object-cover py-2"
+							width={500}
+							height={500}
+							priority
+						/>
+					</div>
+					<ProfileInfoCard data={data} />
 				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
