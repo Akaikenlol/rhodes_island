1e76ef4f3a7249ac01f50266419edfea4b20f4dd
Akaikenlol <keithken972@gmail.com>
2024-03-18 21:28:35 +0630

    feat: Add filterType parameter to fetchOperatorBaseOnDate function

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,16 @@
-import Filter from "@/components/shared/Filter";
 import Header from "@/components/shared/Header";
-import Search from "@/components/shared/Search";
-import { Filters } from "@/constants";
 import { fetchOperatorBaseOnDate } from "@/lib/actions/action";
 import { SearchParamsProps } from "@/types";
 
 export default async function Home({ searchParams }: SearchParamsProps) {
-	const data = await fetchOperatorBaseOnDate({
-		searchQuery: searchParams.q || "",
-	});
+	const filterType = searchParams ? searchParams["filter"] : null;
+	const data = await fetchOperatorBaseOnDate(
+		{
+			searchQuery: searchParams.q || "",
+			filterType: searchParams.filter,
+		},
+		searchParams.filter || ""
+	);
 	return (
 		<section className="sm:p-10 py-10 px-8 flex flex-col gap-10 ">
 			<Header searchParams={searchParams} />
